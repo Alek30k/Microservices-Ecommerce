b6d09bd0d86af8bf1da52faf91169e389c7c99f8
Alek30k <alemank23k@gmail.com>
2026-01-25 16:35:09 -0300

    last commit

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,10 +1,5 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
-// import { clerkMiddleware, getAuth } from "@clerk/express";
-// import { shouldBeUser } from "./middleware/authMiddleware.js";
-// import productRouter from "./routes/product.route";
-// import categoryRouter from "./routes/category.route";
-// import { consumer, producer } from "./utils/kafka.js";
 
 const app = express();
 app.use(
@@ -14,29 +9,14 @@ app.use(
   }),
 );
 app.use(express.json());
-// app.use(clerkMiddleware());
 
-// app.get("/health", (req: Request, res: Response) => {
-//   return res.status(200).json({
-//     status: "ok",
-//     uptime: process.uptime(),
-//     timestamp: Date.now(),
-//   });
-// });
-
-// app.get("/test", shouldBeUser, (req, res) => {
-//   res.json({ message: "Product service authenticated", userId: req.userId });
-// });
-
-// app.use("/products", productRouter);
-// app.use("/categories", categoryRouter);
-
-// app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-//   console.log(err);
-//   return res
-//     .status(err.status || 500)
-//     .json({ message: err.message || "Inter Server Error!" });
-// });
+app.get("/health", (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
 
 app.listen(8000, () => {
   console.log("Product service is running on 8000");
