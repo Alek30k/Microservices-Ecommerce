a64864d3914c23387cde9bfd3c894b74f9915faf
Alek30k <alemank23k@gmail.com>
2026-02-09 11:01:12 -0300

    last commit

--- apps/client/src/app/test/page.tsx
+++ apps/client/src/app/test/page.tsx
@@ -1,5 +1,16 @@
+import { auth } from "@clerk/nextjs/server";
+
 const TestPage = async () => {
-  const res = await fetch("http://localhost:8000/test");
+  const { getToken } = await auth();
+  const token = await getToken();
+
+  console.log(token);
+
+  const res = await fetch("http://localhost:8000/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   const data = await res.json();
 
   console.log(data);
--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -1,14 +1,15 @@
 import Fastify from "fastify";
-// import Clerk from "@clerk/fastify";
 // import { shouldBeUser } from "./middleware/authMiddleware.js";
 // import { connectOrderDB } from "@repo/order-db";
 // import { orderRoute } from "./routes/order.js";
 // import { consumer, producer } from "./utils/kafka.js";
 // import { runKafkaSubscriptions } from "./utils/subscriptions.js";
 
+import { clerkPlugin, getAuth } from "@clerk/fastify";
+
 const fastify = Fastify();
 
-// fastify.register(Clerk.clerkPlugin);
+fastify.register(clerkPlugin);
 
 fastify.get("/health", (request, reply) => {
   return reply.status(200).send({
@@ -18,12 +19,13 @@ fastify.get("/health", (request, reply) => {
   });
 });
 
-// fastify.get("/test", { preHandler: shouldBeUser }, (request, reply) => {
-//   return reply.send({
-//     message: "Order service is authenticated!",
-//     userId: request.userId,
-//   });
-// });
+fastify.get("/test", (request, reply) => {
+  const { isAuthenticated, userId } = getAuth(request);
+  if (!userId) {
+    return reply.send({ message: "You are not logged in" });
+  }
+  return reply.send({ message: "Order service is authenticated!" });
+});
 
 // fastify.register(orderRoute);
 
